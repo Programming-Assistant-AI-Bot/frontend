bae72f7d91634d741ff9350cd6a6f78c3aafbbab
R.Sinthujan <sinthujanp2135@gmail.com>
2025-03-22 13:25:30 +0530

    SignUpAdditionalInfo page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Chatbot from '@/components/chatBot/Chatbot';
 import './App.css'
 import SignUpBaseInfo from '@/components/signUp/SignUpBaseInfo';
 import { Route,Routes } from 'react-router-dom';
+import SignUpAdditionalInfo from './components/signup/SignUpAdditionalInfo';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Chatbot/>}/>
         <Route path='/signup' element={<SignUpBaseInfo/>}/>
+        <Route path='/signup2' element={<SignUpAdditionalInfo/>}/>
       </Routes>
     </>
   )
--- src/components/signup/SignUpAdditionalInfo.jsx
+++ src/components/signup/SignUpAdditionalInfo.jsx
@@ -3,7 +3,7 @@ import bgImage from '@/assets/images/bgImage.png'
 import logo from '@/assets/images/logo.png'
 import styles from '@/components/signup/SignUp.module.css'
 
-function SignUpBaseInfo() {
+function SignUpAdditionalInfo() {
   return (
     <div className={styles.pageContainer}>
       <div className='absolute bottom-0 left-0 right-0 h-[45%] bg-cover' style={{backgroundImage: `url(${bgImage})`}}/>
@@ -12,32 +12,34 @@ function SignUpBaseInfo() {
           <img src={logo} alt="logo" className='w-full max-h-full object-fill'  />
         </div>
         <div className='h-[80%] w-full'>
-          <h1 className="text-3xl font-bold text-center m-9">Sign Up</h1>
+          <h1 className="text-3xl font-bold text-center m-8">Sign Up</h1>
           <form id="signupForm">
             <div class="grid grid-cols-2 gap-4">
                 <div>
-                    <label class="text-sm font-semibold">First Name</label>
-                    <input type="text" id="firstName" className={styles.formInput} placeholder="First Name" required/>
+                    <label class="text-sm font-semibold">Address</label>
+                    <input type="text" id="firstName" className={styles.formInput} placeholder="No" required/>
                 </div>
-                <div>
-                    <label class="text-sm font-semibold">Last Name</label>
-                    <input type="text" id="lastName" className={styles.formInput} placeholder="Last Name" required/>
+                <div class='pt-6'>
+                    <input type="text" id="lastName" className={styles.formInput} placeholder="Street" required/>
                 </div>
             </div>
 
-            <div class="mt-6">
-                <label class="text-sm font-semibold">Email</label>
-                <input type="email" id="email" className={styles.formInput} placeholder="Email" required/>
+            <div class="mt-2">
+                <input type="email" id="text" className={styles.formInput} placeholder="City" required/>
+            </div>
+
+            <div class="mt-2">
+                <input type="password" id="text" className={styles.formInput} placeholder="Country" required/>
             </div>
 
             <div class="mt-6">
-                <label class="text-sm font-semibold">Password</label>
-                <input type="password" id="password" className={styles.formInput} placeholder="Password" required/>
+                <label class="text-sm font-semibold">DOB</label>
+                <input type="date" id="text" className={styles.formInput} placeholder="DOB" required />
             </div>
 
            
             <button type="submit" className={styles.submitButton}>
-                Next
+                Sign Up
             </button>
         </form>
         <div className={styles.loginLink}>Log in?</div>
@@ -48,4 +50,4 @@ function SignUpBaseInfo() {
   )
 }
 
-export default SignUpBaseInfo
+export default SignUpAdditionalInfo
